c9b0279c8beda6343a1d7044934cb3042861e0a2
nazarukroman <nazarukroman@bk.ru>
2017-11-26 21:07:37 +0300

    adverts

--- js/map.js
+++ js/map.js
@@ -2,14 +2,15 @@
 var similarAdverts = [];
 var map = document.querySelector('.map');
 var flats = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
-var features = {
+var types = {
   flat: 'Квартира',
   house: 'Дом',
   bungalo: 'Бунгало'};
-var facilities = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var times = ['12:00', '13:00', '14:00'];
 var mapPins = document.querySelector('.map__pins');
 var pinTemplate = document.querySelector('template').content.querySelector('.map__card');
+var buttonTemplate = document.querySelector('template').content.querySelector('.map__pin');
 
 /**
  * Находим случайное число из диапазона
@@ -34,6 +35,32 @@ var randomArrayValue = function (array) {
   return arrayValue;
 };
 
+/**
+ * Копируем массив facilities, сортируем его случайным образом, выбираем случайную длину массива от 1 до 6
+ * @method
+ * @return {[array]} [Возвращаем новый массив]
+ */
+var randomFeatures = function () {
+
+  /**
+   * [Число для случайного сравнения]
+   * @method
+   * @param  {[type]} a [description]
+   * @param  {[type]} b [description]
+   * @return {[type]} [description]
+   */
+  var compareRandom = function (a, b) {
+    return Math.random() - 0.5;
+  };
+
+  var newFeatures = features.slice(0);
+  newFeatures.sort(compareRandom);
+  newFeatures.length = randomNumber(1, 5);
+  // var stringFeatures = newFeatures.join(', ');
+
+  return newFeatures;
+};
+
 var someX;
 var someY;
 var addressString;
@@ -43,12 +70,13 @@ var randomAddress = function () {
   addressString = [someX, someY];
 
   return addressString;
-}
+};
 randomAddress();
 
 /**
  * Функция которая генерирует объект
  * @method
+ * @param  {[number]} a [Индекс]
  * @return {[object]} [Типовой объект]
  */
 var typicalObject = function (a) {
@@ -59,14 +87,14 @@ var typicalObject = function (a) {
 
     'offer': {
       'title': flats[a],
-      'address': addressString,
+      'address': addressString.join(', '),
       'price': randomNumber(1000, 1000000),
-      'type': randomArrayValue(Object.values(features)),
+      'type': randomArrayValue(Object.values(types)),
       'rooms': randomNumber(1, 5),
       'guests': randomNumber(5, 50),
       'checkin': randomArrayValue(times),
       'checkout': randomArrayValue(times),
-      'features': randomArrayValue(facilities),
+      'features': randomFeatures(),
       'description': '',
       'photos': []
     },
@@ -79,14 +107,46 @@ var typicalObject = function (a) {
   return someObject;
 };
 
+/**
+ * Удаляем элементы списка popup__features
+ * @method
+ */
+var removeCh = function () {
+  for (var i = 0; i < 6; i++) {
+    popupList.removeChild(elementList[i]);
+  }
+};
+
+/**
+ * Добавляем в html в popup__features список удобств из объекта similarAdverts.offer.features
+ * @method
+ */
+var featuresHtml = function () {
+  for (var j = 0; j < similarAdverts[i].offer.features.length; j++) {
+    var elementListHtml = '<li class="feature feature--' + similarAdverts[i].offer.features[j] + '"></li>';
+    popupList.insertAdjacentHTML('afterbegin', elementListHtml);
+  }
+};
+
 for (var i = 0; i < 8; i++) {
   similarAdverts[i] = typicalObject(i);
-};
+}
 
 map.classList.remove('map--faded');
 
 for (var i = 0; i < similarAdverts.length; i++) {
+  var mapPoint = buttonTemplate.cloneNode(true);
+  mapPoint.style.left = similarAdverts[i].location.x + 'px';
+  mapPoint.style.left = similarAdverts[i].location.y + 'px';
+  mapPoint.querySelector('img').src = similarAdverts[i].author.avatar;
+
+  mapPins.appendChild(mapPoint);
+}
+
+for (var i = 0; i < similarAdverts.length; i++) {
   var somePin = pinTemplate.cloneNode(true);
+  var popupList = somePin.querySelector('.popup__features');
+  var elementList = somePin.querySelectorAll('.popup__features .feature');
 
   somePin.querySelector('.popup__avatar').src = similarAdverts[i].author.avatar;
   somePin.querySelector('h3').textContent = similarAdverts[i].offer.title;
@@ -95,13 +155,9 @@ for (var i = 0; i < similarAdverts.length; i++) {
   somePin.querySelector('h4').textContent = similarAdverts[i].offer.type;
   somePin.querySelectorAll('p')[2].textContent = similarAdverts[i].offer.rooms + ' комнаты для ' + similarAdverts[i].offer.guests + ' гостей';
   somePin.querySelectorAll('p')[3].textContent = 'Заезд после ' + similarAdverts[i].offer.checkin + ' , выезд до ' + similarAdverts[i].offer.checkout;
+  removeCh();
+  featuresHtml();
+  somePin.querySelectorAll('p')[4].textContent = similarAdverts[i].offer.description;
 
   mapPins.appendChild(somePin);
-};
-
-
-// var renderPins = function () {
-//   var somePin = pinTemplate.cloneNode();
-//
-//   return somePin;
-// };
+}
