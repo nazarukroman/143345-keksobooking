2256d7574e25ede86906c18a7b0a286ca7fc3c18
Roman Nazaruk <nazarukroman@bk.ru>
2017-12-25 08:32:00 +0300

    modules

--- js/map.js
+++ js/map.js
@@ -1,24 +1,24 @@
 'use strict';
 
-var map = document.querySelector('.map');
+(function () {
+  var map = document.querySelector('.map');
+  var mapPins = document.querySelector('.map__pins');
+  var mainPin = document.querySelector('.map__pin--main');
+  /**
+   * Функция для
+   * Удаления затемнения карты
+   * Отрисовывания объявления
+   * Удаления атрибута disabled с полей формы
+   */
+  var mainPinMouseupHandler = function () {
+    map.classList.remove('map--faded');
+    mapPins.appendChild(window.pin.fragment());
+    window.form.removeDisabled();
+    window.form.noticeForm.classList.remove('notice__form--disabled');
+  };
 
-
-var mapPins = document.querySelector('.map__pins');
-var mainPin = document.querySelector('.map__pin--main');
-/**
- * Функция для
- * Удаления затемнения карты
- * Отрисовывания объявления
- * Удаления атрибута disabled с полей формы
- */
-var mainPinMouseupHandler = function () {
-  map.classList.remove('map--faded');
-  mapPins.appendChild(window.pin.fragment());
-  window.form.removeDisabled();
-  window.form.noticeForm.classList.remove('notice__form--disabled');
-};
-
-/**
- * Обработчик события(нажатия мышкой) по главной метке на карте
- */
-mainPin.addEventListener('mouseup', mainPinMouseupHandler);
+  /**
+   * Обработчик события(нажатия мышкой) по главной метке на карте
+   */
+  mainPin.addEventListener('mouseup', mainPinMouseupHandler);
+})();
